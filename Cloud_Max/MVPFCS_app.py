8bd95e9def70a7dcf4d4375c2a28cde2452b78b2
Farley C Sardinha <fcsardinha@gmail.com>
2025-08-14 14:39:22 +0000

    Primeira versão do app

--- Cloud_Max/MVPFCS_app.py
+++ Cloud_Max/MVPFCS_app.py
@@ -1,1 +1,18 @@
+import streamlit as st
 
+st.title("👨🏻‍🏫👩🏽‍🏫 Docentes do ES")
+st.write(
+    "**Primeira versão do MVP**"
+)
+st.markdown("""
+* **Nome:** Farley
+* **Tema:** Docentes do ES segundo o Censo Escolar
+* **Seções:**
+* **Apresentação**
+* **Distribuição de docentes segundo o município por:**
+    * etapa e dependência
+    * gênero e faixa etária
+    * nível de escolaridade e formação acadêmica
+    * situação funcional e dependência administrativa
+* **Base de Dados:** Sinopse do Censo Escolar 2024
+""")
