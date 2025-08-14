eae27b16130bd3df9469cf3c92689f3e55cd27c9
Farley C Sardinha <fcsardinha@gmail.com>
2025-08-14 14:51:34 +0000

    Corrigindo indentação

--- Cloud_Max/MVPFCS_app.py
+++ Cloud_Max/MVPFCS_app.py
@@ -8,11 +8,11 @@ st.markdown("""
 * **Nome:** Farley
 * **Tema:** Docentes do ES segundo o Censo Escolar
 * **Seções:**
-* **Apresentação**
-* **Distribuição de docentes segundo o município por:**
-    * etapa e dependência
-    * gênero e faixa etária
-    * nível de escolaridade e formação acadêmica
-    * situação funcional e dependência administrativa
+    * **Apresentação**
+    * **Distribuição de docentes segundo o município por:**
+        * etapa e dependência
+        * gênero e faixa etária
+        * nível de escolaridade e formação acadêmica
+        * situação funcional e dependência administrativa
 * **Base de Dados:** Sinopse do Censo Escolar 2024
 """)
